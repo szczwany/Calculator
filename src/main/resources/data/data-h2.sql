a50f7af8c17ba53a41614e254fe256ba1fa3b194
Tomek-Komputer\Tomek <szczwany@hotmail.com>
2018-01-02 18:17:10 +0100

    Initial test data sql script changed

--- src/main/resources/data/data-h2.sql
+++ src/main/resources/data/data-h2.sql
@@ -1,5 +1,7 @@
 INSERT INTO projects (name) VALUES
-('dodawanie');
+('Simple calculations'), ('Population density /km2'), ('100 PLN in foreign currencies');
 
 INSERT INTO calculations (description, expression, project_id) VALUES
-('pierwsza kalkulacja','2+2', 1), ('druga kalkulacja', '123+434*2/33', 1),('trzecia kalkulacja', '2+2*2', 1);
+('First calculation','2+2', 1), ('Second calculation', '12+434*21/3.3', 1), ('Third calculation', '2.2+2.89*2.98', 1),
+('Poland','38422346/312679', 2), ('Germany', '82349400/357375.62', 2), ('Japan', '126451398/377972', 2),
+('China','1379302771/9596960', 2), ('GBP', '100/4.7', 3), ('EUR', '100/4.16', 3), ('USD', '100/3.45', 3);
